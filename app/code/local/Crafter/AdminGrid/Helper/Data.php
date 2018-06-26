3b764927faffffeb045988e484a7d2ca3e2eec66
crafter <adam@crafter.net.pl>
2018-06-26 21:38:18 +0200

    dasf

---
+++ app/code/local/Crafter/AdminGrid/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+class Crafter_AdminGrid_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    //https://bsscommerce.com/confluence/3-simple-steps-to-create-admin-grid-in-magento-1.html
+}
+	 
